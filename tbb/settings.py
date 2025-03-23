5933c261cba4ba774fa983ed2ae5cfe6227271b6
Okpara007 <chinemeremokpara93@gmail.com>
2025-03-23 10:05:25 +0100

    added allowed host and switching to my branch

--- tbb/settings.py
+++ tbb/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [ 'localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'trade-by-barter-web-app.onrender.com', 'www.trade-by-barter-web-app.onrender.com']
 
 # Application definition
 
